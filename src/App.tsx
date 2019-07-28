c1f7cd768907afeba3ada9fb2102e316b7c7973b
Andrew Head <head.andrewm@gmail.com>
2019-07-27 17:16:39 -0700

    App: Proof of concept of updating state of App externally

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface AppState {
+  message: string
+}
+
+class App extends React.Component<{}, AppState> {
+
+  constructor() {
+    super({});
+    this.state = {
+      message: "Starter message"
+    };
+  }
+
   public render() {
     return (
       <div className="App">
@@ -13,6 +25,9 @@ class App extends React.Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          Additional message: { this.state.message }
         </p>
       </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,21 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+window.addEventListener('message', (event) => {
+  console.log("Message received");
+});
+
+declare global {
+  interface Window { app: App; }
+}
+
 ReactDOM.render(
-  <App />,
+  <App ref={(component) => { if (component !== null) { window.app = component; }}} />,
   document.getElementById('root') as HTMLElement
 );
+
+setTimeout(() => {
+  if (window.app !== undefined) {
+    window.app.setState({ message: "Updated message" });
+  }
+}, 3000);

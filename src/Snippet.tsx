c2344b43b4c733c3e59ac73e6f6dc40fb201e4fa
Andrew Head <head.andrewm@gmail.com>
2019-08-07 01:29:22 -0700

    Bugfix: Make important structures global; import React in Snippet file

--- src/Snippet.tsx
+++ src/Snippet.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 export const Snippet = () => {
   return <div className="Snippet">
     This is a snippet.
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import * as santokuEditorAdapterClasses from "santoku-editor-adapter";
 import { store } from "santoku-store";
-import './index.css';
-import Santoku from './Santoku';
+import "./index.css";
+import Santoku from "./Santoku";
+
+declare global {
+  interface Window {
+    santoku: any;
+  }
+}
 
 ReactDOM.render(
   <Provider store={store}>
     <Santoku />
   </Provider>,
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
-export * from "santoku-editor-adapter";
+/**
+ * Assign Santoku globals to let IDE extensions take care of setup.
+ */
+window.santoku = {
+  ...santokuEditorAdapterClasses,
+  store
+};
